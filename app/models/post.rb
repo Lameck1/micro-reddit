77805c488d9685a302ec1b2ea7bef93a2ac9ffdc
Lameck1 <lameck72@gmail.com>
2020-12-14 18:28:57 +0300

    Validate Post and User
    Added validations to Post and User as follows:
    
    - For posts, title, body and user_id must be present
    - For user, first and last name must be present and not exceed 12
    characters each. Email and Password must match the provided regular
    expressions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, :body, :user_id, presence: true
   belongs_to :user
   has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
-    has_many :posts 
+    EMAIL_REGEX = /\A[a-z0-9._&+-]+@[a-z0-9._&+-]+\.[a-z]{2,4}\Z/i.freeze
+    PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/
+    validates :first_name, :last_name, presence: true, length: { maximum: 12 }
+    validates :email, presence: true,
+                    length: { maximum: 20 },
+                    format: { with: EMAIL_REGEX },
+                    confirmation: true
+    validates :password, presence: true,
+                        format: { with: PASSWORD_REGEX }
+
+    has_many :posts
+    has_many :comments
 end
