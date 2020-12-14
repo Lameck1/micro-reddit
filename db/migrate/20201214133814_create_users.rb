3248e2c28c98bd2d5af2a1305bd7a94b026aaa3e
Delica Lydia <Delydia84@gmail.com>
2020-12-14 16:19:09 +0200

    Change password data type: Changed password data type from string to password

--- db/migrate/20201214133814_create_users.rb
+++ db/migrate/20201214133814_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.string :password
+      t.password :password
 
       t.timestamps
     end
